cc06d8a80a94304a0cba7468a690e16ae6162835
Arsalan Khan <arsalanbashirkhan321@gmail.com>
2024-01-22 11:58:05 +0530

    console deleted

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
   const options = currencyInfo ? Object.keys(currencyInfo) : [];
 
   const swap = () => {
-    
     setFrom(to)
     setTo(from)
     setConvertedAmount(amount)
